b5d2bdff8891bc9069207b237ba32a830e397be9
asawant01 <130009253+asawant01@users.noreply.github.com>
2023-04-15 14:58:06 -0400

    Add files via upload

--- loan.py
+++ loan.py
@@ -3,9 +3,9 @@ def calculate_emi(p,n,r):
 	emi = p*(r/100)*((1+(r/100)**n)/(1+(r/100)**n)-1)
 	return emi
 st.title('EMI calculator')
+p = st.slider('Enter Principal Amount', 1,1000000)
+n = st.slider('Enter number of years', 1,20)
+r = st.slider('Enter rate of interest', 1,25)
 if st.button('Calculate'):
-	p = st.slider('Enter Principal Amount', 1,1000000)
-	n = st.slider('Enter number of years', 1,20)
-	r = st.slider('Enter rate of interest', 1,25)
 	emi = calculate_emi(p,n,r)
 	st.write('EMI =',emi)
